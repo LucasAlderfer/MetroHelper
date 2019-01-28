34d947f01cfc17c1dad542d96e0edaf6aed16248
Lucas Alderfer <lucas.alderfer@gmail.com>
2019-01-28 14:24:06 -0700

    WIP Adds all stations on a line to the line page

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,27 +1,29 @@
 class LinesController < ApplicationController
 
   def Red
-
+    @red_stations = Line.find_by_name('red').stations
   end
 
   def Blue
-
+    blue_line = Line.find_by_name('blue')
+    binding.pry
+    @blue_stations = blue_line.stations
   end
 
   def Green
-
+    @green_stations = Line.find_by_name('green').stations
   end
 
   def Yellow
-
+    @yellow_stations = Line.find_by_name('yellow').stations
   end
 
   def Silver
-
+    @silver_stations = Line.find_by_name('silver').stations
   end
-  
-  def Orange
 
+  def Orange
+    @orange_stations = Line.find_by_name('orange').stations
   end
 
 end
---
+++ spec/features/user_can_see_all_stations_on_a_line_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe 'visiting /red' do
+  context 'as a user' do
+    it 'can see all stations on the red line' do
+      StationsAndLinesService.new.create_lines
+      StationsAndLinesService.new.create_stations
+      StationsAndLinesService.new.create_line_stations
+
+      visit '/red'
+
+      expect(page).to have_css('.station', count: 28)
+
+      visit '/blue'
+
+      expect(page).to have_css('.station', count: 29)
+
+      visit '/green'
+
+      expect(page).to have_css('.station', count: 20)
+
+      visit '/silver'
+
+      expect(page).to have_css('.station', count: 30)
+
+      visit '/orange'
+
+      expect(page).to have_css('.station', count: 28)
+
+      visit '/yellow'
+
+      expect(page).to have_css('.station', count: 16)
+    end
+  end
+end
